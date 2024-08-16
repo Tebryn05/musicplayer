b68e839eea8a3e46e9386cf293f8b3c84c97c803
Tebryn05 <158665169+Tebryn05@users.noreply.github.com>
2024-08-16 13:26:07 -0400

    Add files via upload

--- musicplayer.py
+++ musicplayer.py
@@ -1,31 +1,52 @@
 from tkinter import *
+from tkinter import filedialog
 import pygame
 import os
 
-def play(audio):
-    audio.play()
+# function to select audio
+def select_audio(audio_file):
 
+    pygame.mixer.music.stop()
+    audio_file = filedialog.askopenfilename(filetypes=[("Audio Files", ".mp3 .ogg .wav")])
+
+    
+    if audio_file:
+        pygame.mixer.music.load(audio_file)
+
+# main function
 def main():
-    pygame.init()
+    pygame.init() # initalize pygame
 
+    # initalize the mixer for pygame
     pygame.mixer.init()
 
+    # create window
     root = Tk()
 
-    root.title("Music Player Test")
+    # attributes of the window
+    root.title("Music Player")
     root.geometry("300x125")
     root.resizable(False, False)
 
-    testMusic = pygame.mixer.Sound("sdre48.mp3")
+    # define audio_file
+    audio_file = ""
 
-    play = Button(root, text="play", width = 5, font=("Courier New", 15), command=lambda: testMusic.play())
+    # play button
+    play = Button(root, text="play", width = 5, font=("Courier New", 15), command=lambda: pygame.mixer.music.play())
 
     play.place(x=120,y=10)
 
-    stop = Button(root, text="stop", width = 5, font=("Courier New", 15), command=lambda: testMusic.stop())
-
+    # stop button
+    stop = Button(root, text="stop", width = 5, font=("Courier New", 15), command=lambda: pygame.mixer.music.stop())
 
     stop.place(x=120, y=62)
+
+    # button to browse files
+    browseFiles = Button(root, text="Browse Files...", width = 15, font=("Courier New", 8), command=lambda: select_audio(audio_file))
+
+    browseFiles.place(x=0, y=0)
+
+    # main loop for the window
     root.mainloop()
 
 main()
